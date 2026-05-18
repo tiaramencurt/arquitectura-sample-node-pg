26d3643f97034580f63a5626309de59b91033f2b
Android Studio <devandroid@ort>
2026-05-18 08:48:49 -0300

    materias.service

---
+++ src/services/materias-service.js
@@ -0,0 +1,43 @@
+import MateriasRepository from '../repositories/materias-repository.js';
+
+export default class MateriasService {
+    constructor() {
+        console.log('Estoy en: MateriasService.constructor()');
+        this.MateriasRepository = new MateriasRepository();
+    }
+
+    //Conecta repository con la query (Materias desde la BD a materias con prog)
+
+    getAllAsync = async () => {
+        console.log(`MateriasService.getAllAsync()`);
+        const returnArray = await this.MateriasRepository.getAllAsync();
+        return returnArray;
+    }
+
+    getByIdAsync = async (id) => {
+        console.log(`MateriasService.getByIdAsync(${id})`);
+        const returnEntity = await this.MateriasRepository.getByIdAsync(id);
+        return returnEntity;
+    }
+
+    createAsync = async (entity) => {
+        console.log(`MateriasService.createAsync(${JSON.stringify(entity)})`);
+        if (!entity?.nombre || entity.nombre.trim() === '') {
+            throw new Error(`El nombre de la materia es obligatorio.`);
+        }
+        const newId = await this.MateriasRepository.createAsync(entity);
+        return newId;
+    }
+
+    updateAsync = async (entity) => {
+        console.log(`MateriasService.updateAsync(${JSON.stringify(entity)})`);
+        const rowsAffected = await this.MateriasRepository.updateAsync(entity);
+        return rowsAffected;
+    }
+
+    deleteByIdAsync = async (id) => {
+        console.log(`MateriasService.deleteByIdAsync(${id})`);
+        const rowsAffected = await this.MateriasRepository.deleteByIdAsync(id);
+        return rowsAffected;
+    }
+}
